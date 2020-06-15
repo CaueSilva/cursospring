0529de5e5dbe2636098236677d8c9fbe3b88f497
Cauê Silva <c_aue_2007@hotmail.com>
2020-06-15 20:03:07 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/curso/cursospring/services/PedidoService.java
@@ -0,0 +1,25 @@
+package br.com.curso.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.curso.cursospring.domain.Pedido;
+import br.com.curso.cursospring.repositories.PedidoRepository;
+import br.com.curso.cursospring.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired //instância automática do Spring
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> Pedido = repo.findById(id);
+		return Pedido.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado. Id: "+id+", tipo: "+Pedido.class.getName()));
+	}
+	
+
+}
