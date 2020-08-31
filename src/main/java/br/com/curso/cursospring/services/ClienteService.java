d4bfcd10df78b72fee02cfa250a5eb88c78605b4
Cauê Silva <c_aue_2007@hotmail.com>
2020-08-31 11:42:59 -0300

    Ajustanto e testando a deleção de Cliente

--- src/main/java/br/com/curso/cursospring/services/ClienteService.java
+++ src/main/java/br/com/curso/cursospring/services/ClienteService.java
@@ -60,7 +60,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir o cliente, há entidades relacionadas.", e);
+			throw new DataIntegrityException("Não é possível excluir, há pedidos relacionados.", e);
 		}
 	}
 	
