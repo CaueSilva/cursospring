9f527c44b8162dd153d1d358594bcf9e9766bf0c
Cauê Silva <c_aue_2007@hotmail.com>
2020-09-03 11:49:34 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/br/com/curso/cursospring/config/DevConfig.java
@@ -0,0 +1,34 @@
+package br.com.curso.cursospring.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.curso.cursospring.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	//Recuperando o valor de spring.jpa.hibernate.ddl-auto que cria/atualiza/dropa o BD na variável strategy
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		//Se o banco de dados já foi criado e preenchido, retorna false
+		if(!strategy.equals("create")) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+}
